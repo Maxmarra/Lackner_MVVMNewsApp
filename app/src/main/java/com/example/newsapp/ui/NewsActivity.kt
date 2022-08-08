5b2c76c4c1fe0f3cfd3d76e6df495f73e388f395
Maxmarra <5919669@gmail.com>
2022-08-08 15:17:54 +0300

    starter with navigation + bottom navigation

--- app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -1,9 +1,8 @@
 package com.example.newsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
 import kotlinx.android.synthetic.main.activity_news.*
