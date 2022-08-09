ddb1089d63d14fd786b5eb104f7560f561598275
Maxmarra <5919669@gmail.com>
2022-08-09 08:51:23 +0300

    02 retrofit setup

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "da552a64d5a44ad19de7d48c95ea3e3a"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
