f7659e682baf9136df7c2b3e57d0406bdc340afc
Maxmarra <5919669@gmail.com>
2022-08-09 09:07:18 +0300

    03_Room_setup

--- app/src/main/java/com/example/newsapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/newsapp/api/RetrofitInstance.kt
@@ -10,11 +10,15 @@ class RetrofitInstance {
     companion object {
 
         private val retrofit by lazy {
+
+            //это спецом для debugging
+            //чтобы просматривать запросы и ответы
             val logging = HttpLoggingInterceptor()
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
             val client = OkHttpClient.Builder()
                 .addInterceptor(logging)
                 .build()
+
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
